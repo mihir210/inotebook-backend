cfdf553a5b15d1ce0b5aefb2aa5a1959e02d7352
mihir hadavani <miheerhadvani990@gmail.com>
2022-08-15 17:27:52 +0530

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "mihir";
 
 const router = express.Router();
 
@@ -21,16 +23,24 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "sorry a user with this email already exists" })
         }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password
-            })
-            .then(user => res.json(user))
-            // .catch(err => {
-            //     console.log(err)
-            //     res.json({ error: 'please enter a unique value for email', massage: err.message })
-            // })
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass
+        });
+        const data = {
+            id: user.id
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+
+        // res.json(user)
+        res.json({ authtoken });
+        // .catch(err => {
+        //     console.log(err)
+        //     res.json({ error: 'please enter a unique value for email', massage: err.message })
+        // })
     } catch (error) {
         console.log(error.message);
         res.status(500).send("some error occured");
